1dc5f484b80065bd73cf22af24a6fb1bf096dde2
P <P@P-PC>
2021-09-25 13:43:07 +0200

    commit 3

--- src/sk/main.java
+++ src/sk/main.java
@@ -5,6 +5,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hahaha");
         //pridal som koment
+        //pridam novu metodu tlace
+        System.out.println("tlac");
     }
     
 }
